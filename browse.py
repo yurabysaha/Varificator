fffacd86e531e3eccaa4aeb0d73190443f39d703
yurabysaha <yurabysaha@gmail.com>
2016-07-22 10:35:34 +0300

    working good v 0.1

---
+++ browse.py
@@ -0,0 +1,46 @@
+import os
+import tkMessageBox
+import tkSimpleDialog
+import Tkinter as tk
+from tkFileDialog import askopenfilename
+import platform
+import xml.etree.ElementTree as ET
+
+from xls2xml import toXML
+
+
+def xmlpath():
+    if platform.system() == "Linux":
+        return '/'
+    else:
+        return '\\'
+
+ROOT_PATH=os.getcwd() + xmlpath()
+
+
+def open_file_handler():
+    filePath = askopenfilename()
+
+    doc = ET.parse(ROOT_PATH + 'file.xml')
+    root = doc.getroot()
+    django = doc.find('filePath')
+    django.text = filePath
+    doc.write(ROOT_PATH + 'file.xml', encoding="utf-8", xml_declaration=True)
+    toXML(filename=filePath,node_name='node', cellsAs='attributes')
+    tkMessageBox.showinfo(
+    "Updated",
+    "File added"
+)
+
+
+class Browse:
+    def __init__(self, body):
+        #butfont = font.Font(family='Ubuntu', size=10)
+        self.but = tk.Button(body, command=open_file_handler, padx=100, text="Browse xls file", bg='#C0C0C0')
+        self.but.grid(row = 1, column = 2)
+
+        # title = tk.Label(body, text="Picture", font="Arial 12")
+        # title.grid(row = 1, column = 1)
+
+
+
---
+++ xls2xml.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python
+import platform
+import os
+import os, sys, inspect, argparse
+# Not sure if this bit is really needed:
+
+# realpath() with make your script run, even if you symlink it :)
+cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile( inspect.currentframe() ))[0]))
+if cmd_folder not in sys.path:
+   sys.path.insert(0, cmd_folder)
+
+# Info:
+# cmd_folder = os.path.dirname(os.path.abspath(__file__)) # DO NOT USE __file__ !!!
+# __file__ fails if script is called in different ways on Windows
+# __file__ fails if someone does os.chdir() before
+# sys.argv[0] also fails because it doesn't not always contains the path
+
+from library import xlrd
+
+def xmlpath():
+    if platform.system() == "Linux":
+        return '/'
+    else:
+        return '\\'
+
+ROOT_PATH=os.getcwd() + xmlpath()
+
+def toXML(filename, node_name, cellsAs):
+    xlsFile = xlrd.open_workbook(filename.encode(sys.getfilesystemencoding() ))
+    firstSheet = xlsFile.sheet_by_index(0)
+    attributes = firstSheet.row_values(0)
+    text_file = open(ROOT_PATH + "Link.txt", "w")
+    for rownum in range(1, firstSheet.nrows):
+        firstSheet.row_values(rownum) 
+        cells = [ convertFloatsToIntStrings(i) for i in firstSheet.row_values(rownum) ]
+        if (cellsAs == "attributes"):
+            s = xmlRowAsAttributes(node_name, attributes, cells, rownum)
+        elif (cellsAs == "nodes"):
+            s = xmlRowAsNodes(node_name, attributes, cells, rownum)
+        print s.encode('utf8')
+        text_file.write(s.encode('utf8') + '\n')
+    text_file.close()
+
+def xmlRowAsAttributes(node_name, attributes, cells, rownum):
+    s = "<" + node_name + " "
+    for index in range(len(attributes)):
+        s += sanitizeAttributeName(attributes[index].strip()) \
+            + "=\"" + cells[index].decode("utf-8").strip() + "\" "
+    s += "/>"
+    return s
+
+def xmlRowAsNodes(node_name, attributes, cells, rownum):
+    s = "<" + node_name + ">\n"
+    rows = []
+    for index in range(len(attributes)):
+        r = "    <" + sanitizeAttributeName(attributes[index].strip())  + ">" \
+            + cells[index].decode("utf-8").strip() \
+            + "</" + sanitizeAttributeName(attributes[index].strip()) + ">\n"
+        rows.append(r)
+    s += "".join(rows)
+    s += "</" + node_name + ">"
+    return s
+
+def convertFloatsToIntStrings(i):
+    if type(i) is float:
+        return str(int(i))
+    elif type(i) is long:
+        return str(int(i))
+    else:
+        return i.encode("utf-8", "replace")
+
+def sanitizeAttributeName(dirtyA):
+    a = dirtyA.replace(" ", "_")
+    a = a.replace("'", "_")
+    a = a.replace('"', "_")
+    a = a.replace("<", "_")
+    a = a.replace(">", "_")
+    return a
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Converts tabular data in Excel spreadsheets into XML.')
+    parser.add_argument('filename', help='Excel (xls or xlsx) file.')
+    parser.add_argument("-n", "--node", help="node for each table row", default="node")
+    parser.add_argument("-r", "--root-node", help="wrap nodes in specified root node")
+    parser.add_argument("-c", "--cells-as", help="use attributes or nodes for cell values", default="attributes", choices=["attributes", "nodes"])
+    args = parser.parse_args()
+    if (args.root_node != None):
+        print "<" + args.root_node + ">"
+    toXML(args.filename, args.node, args.cells_as)
+    if (args.root_node != None):
+        print "</" + args.root_node + ">"
